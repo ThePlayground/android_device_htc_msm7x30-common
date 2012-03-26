8fc1828b572e11b446e813e39f9016929d729277
TwistedUmbrella <twistedumbrella@gmail.com>
2012-03-25 20:16:21 -0400

    Drop excess wireless configurations for eth device interface

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -37,21 +37,16 @@ TARGET_SPECIFIC_HEADER_PATH := device/htc/msm7x30-common/include
 # TARGET_CUSTOM_WIFI := device/htc/msm7x30-common/hardware/wifi/wifi.c
 
 # Merged Wifi Defines
-BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
+WIFI_BAND                        := 802_11_ABG
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
-BOARD_HOSTAPD_DRIVER             := WEXT
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wext
 BOARD_WLAN_DEVICE                := bcm4329
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcm4329/parameters/firmware_path"
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcm4329.ko"
 WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcm4329.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcm4329_apsta.bin"
-WIFI_DRIVER_FW_STA_PATH          := "/vendor/firmware/fw_bcm4329.bin"
-WIFI_DRIVER_FW_AP_PATH           := "/vendor/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcm4329/parameters/firmware_path"
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcm4329.ko"
 WIFI_DRIVER_MODULE_NAME          := "bcm4329"
-BOARD_WLAN_DEVICE_REV            := bcm4329
-WIFI_BAND                        := 802_11_ABGN
 
 # Older Touchscreen Support
 BOARD_USE_LEGACY_TOUCHSCREEN := true
