cce335264925055d008761ad47c110d76e4bab2b
TwistedUmbrella <twistedumbrella@gmail.com>
2012-03-28 11:21:00 -0400

    While you're at it, why not fix wifi tethering for 7x30 devices?

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -47,6 +47,8 @@ WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcm4329_apsta.bin"
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcm4329/parameters/firmware_path"
 WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcm4329.ko"
 WIFI_DRIVER_MODULE_NAME          := "bcm4329"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/proc/calibration iface_name=eth0"
+TARGET_CUSTOM_IFACE := eth0
 
 # Older Touchscreen Support
 BOARD_USE_LEGACY_TOUCHSCREEN := true
